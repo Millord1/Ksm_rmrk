54ae8ca68a06875f02657810d3ba64605824cf9c
Millord <62941190+Millord1@users.noreply.github.com>
2021-02-16 16:29:04 +0100

    modif contract ID for cannonize

--- classes/Rmrk/Interaction.ts
+++ classes/Rmrk/Interaction.ts
@@ -27,8 +27,8 @@ export abstract class Interaction extends Remark implements PublicInteraction
 
         let nftDatas = this.checkDatasLength(computed.split('-'));
 
-        const computedSplit = computed.split('-');
-        const assetId = computedSplit[1] + '-' + computedSplit[2];
+        const sn = computed.split('-')[4];
+        const assetId = computed.replace('-'+sn, '');
 
         return new Asset(this.rmrk, this.chain, this.version, this.transaction, nftDatas, assetId, meta);
     }
@@ -51,36 +51,40 @@ export abstract class Interaction extends Remark implements PublicInteraction
 
         const obj = Remark.entityObj;
 
-        if(this.version === 'RMRK0.1' || this.version === "0.1"){
-            // Not allowed
-
-            let collection: string = "";
-
-            obj.sn = data[data.length -1];
-            data.splice(data.length -1, 1);
-
-            obj.name = data[data.length -1];
-            data.splice(data.length -1, 1);
-
-            for (let i = 0; i<data.length; i++){
-                if(i != data.length-1){
-                    collection += data[i] + '-';
-                }else{
-                    collection += data[i];
-                }
-            }
-
-            obj.collection = collection;
-
-        }
-        else
-            if (this.version === "1.0.0"){
+        // if(this.version === 'RMRK0.1' || this.version === "0.1"){
+        //     // Not allowed
+        //
+        //     let collection: string = "";
+        //
+        //     obj.sn = data[data.length -1];
+        //     data.splice(data.length -1, 1);
+        //
+        //     obj.name = data[data.length -1];
+        //     data.splice(data.length -1, 1);
+        //
+        //     for (let i = 0; i<data.length; i++){
+        //         if(i != data.length-1){
+        //             collection += data[i] + '-';
+        //         }else{
+        //             collection += data[i];
+        //         }
+        //     }
+        //
+        //     obj.collection = collection;
+        //
+        // }
+        // else
+            if (this.version === "1.0.0" || this.version === "RMRK1.0.0"){
             // Normalization
-
+            console.log(data);
             if(data.length === 4){
-                obj.collection = data[1];
+                obj.collection = data[1] + '-' + data[2];
                 obj.name = data[2];
                 obj.sn = data[3];
+            }else if(data.length > 4){
+                obj.collection = data[1] + '-' + data[2];
+                obj.name = data[3];
+                obj.sn = data[4];
             }
 
         }
