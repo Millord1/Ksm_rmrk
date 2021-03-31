676d4a87144cb11a9d8e5a7950a678abdbb81c03
Millord <roffat.theau@outlook.com>
2021-03-31 18:23:39 +0200

    add FileManager.ts

---
+++ src/Files/FileManager.ts
@@ -0,0 +1,138 @@
+const fs = require('fs');
+const path = require('path');
+
+
+export class FileManager
+{
+
+    private static threadLock: string = "thread.lock.json";
+    private static dir: string = __dirname+ "/../Files/";
+
+    // Getters for have path of files
+    private static getSavePath(chainName: string): string
+    {
+        const save: string = "_lastBlock.json";
+        return this.dir + chainName + save;
+    }
+
+    public static getThreadLockPath()
+    {
+        return this.dir + this.threadLock;
+    }
+
+    private static getRescanPath()
+    {
+        return this.dir + "toRescan.json";
+    }
+
+
+    public static startLock(startBlock: number, chain: string)
+    {
+        // create file for lock one thread
+
+        const dateTimestamp = Date.now() * 1000;
+        const date = new Date(dateTimestamp);
+
+        const threadData = {
+            startBlock: startBlock,
+            chain: chain,
+            start: date
+        }
+
+        const data = JSON.stringify(threadData);
+
+        try{
+            fs.writeFileSync(path.resolve(this.getThreadLockPath()), data);
+        }catch(e){
+            console.error(e);
+        }
+
+    }
+
+
+
+    public static checkLock(): boolean
+    {
+        return fs.existsSync(path.resolve(this.getThreadLockPath()));
+    }
+
+
+    public static getLastBlock(chain: string): number|undefined
+    {
+        console.log(path.resolve(this.getSavePath(chain)));
+        // read file for get last block
+        if( fs.existsSync( path.resolve(this.getSavePath(chain)) ) ){
+            const lastBlock = fs.readFileSync( path.resolve(this.getSavePath(chain)) );
+            const data = JSON.parse(lastBlock);
+
+            return data.lastBlock;
+        }
+
+        return undefined
+    }
+
+
+    public static exitProcess(blockNumber: number, chain: string, toRescan: Array<number>)
+    {
+        // save block and exit process
+        console.log('exit process ...');
+        blockNumber--;
+
+        if(this.saveLastBlock(blockNumber, chain)){
+            console.log('saved block : '+blockNumber);
+        }else{
+            console.log('Fail to save block : '+blockNumber);
+        }
+
+        const rescan: string = JSON.stringify(toRescan);
+
+        if(!fs.existsSync(this.getRescanPath())){
+
+            try{
+                fs.writeFileSync(path.resolve(this.getRescanPath()), rescan);
+                console.log("Rescan saved");
+            }catch(e){
+                console.error(e);
+            }
+
+        }else{
+
+            try{
+                const blocks = fs.readFileSync(this.getRescanPath());
+                const oldBlocks: Array<number> = JSON.parse(blocks);
+
+                const newArray: Array<number> = oldBlocks.concat(oldBlocks, toRescan);
+                const toPush = JSON.stringify(newArray);
+
+                fs.writeFileSync(this.getRescanPath(), toPush);
+                console.log("Rescan saved");
+            }catch(e){
+                console.error(e);
+            }
+
+        }
+
+        process.exit();
+    }
+
+
+
+    public static saveLastBlock(lastBlock: number, chain: string): boolean
+    {
+        // write file with last block
+        const saveBlock = {
+            lastBlock: lastBlock
+        }
+
+        const data = JSON.stringify(saveBlock);
+
+        try{
+            fs.writeFileSync(path.resolve( this.getSavePath(chain)), data);
+            return true;
+        }catch(e){
+            console.error(e);
+            return false;
+        }
+
+    }
+}
