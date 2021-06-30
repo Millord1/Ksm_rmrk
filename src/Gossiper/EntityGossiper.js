fabf0d4048412a8aee7068460215ab4ef7961c7f
Millord <roffat.theau@outlook.com>
2021-06-30 10:15:00 +0200

    add .js compiled

--- src/Gossiper/EntityGossiper.js
+++ src/Gossiper/EntityGossiper.js
@@ -80,7 +80,4 @@ class EntityGossiper extends GossiperManager_1.GossiperManager {
     }
 }
 exports.EntityGossiper = EntityGossiper;
-break;
-console.error('Something is wrong with Entity Gossip of ' + this.blockId);
-break;
 //# sourceMappingURL=EntityGossiper.js.map
