da622ca512d19250b7f59354e5035359e3fb5964
Millord <roffat.theau@outlook.com>
2021-09-16 09:09:11 +0200

    delete one wsProvider and modif the second

--- src/Blockchains/Unique.ts
+++ src/Blockchains/Unique.ts
@@ -6,8 +6,7 @@ export class Unique extends GenericBlockchain
 
     public constructor() {
         const firstTestNet: string = "wss://unique.usetech.com";
-        const secondTestNet: string = "wss://testnet2.uniquenetwork.io/";
-        const thirdTestNet: string = "wss://unique-rpc.polkadot.io/";
+        const secondTestNet: string = "wss://testnet2.unique.network";
         super("UNQ", "", true, secondTestNet, 15);
     }
 
